0c0640549009037ad858bbab5a5847aea77cf6bc
Eveliis <eveliis.kallas@khk.ee>
2018-12-03 09:47:21 +0200

    Added loading animation to next page

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+    window.location.assign("login.html");
+    $(document).ready(function(){
+        $('#wrapper').fadeIn();
+    });
+
+}
